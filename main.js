c935d6c8623b47fde3a7ab523b0c1eb9d099cb89
samarzribi <samarzribi014@gmail.com>
2021-08-10 16:31:52 +0100

    still working

--- main.js
+++ main.js
@@ -16,25 +16,44 @@
 // }
 
 
-function Peak(name,price,url){
-    var food={}
+// function Peak(name,price,url){
+//     var food={}
   
-    food.name=name;
-    food.price=price;
-    food.url=url;
-    return food
-}
+//     food.name=name;
+//     food.price=price;
+//     food.url=url;
+//     return food
+// }
 
   
-  var food1=Peak('pizza','  30$', 'pizzas.jpg')
-  var food2=Peak('tacos',' 30$','tacos.jpeg' )
-  var foods= [food1,food2]
+//   var food1=Peak('pizza','  30$', 'pizzas.jpg')
+//   var food2=Peak('tacos',' 30$','tacos.jpeg' )
+//   var foods= [food1,food2]
 
 
 
-var $carrd1=$('#card1')
-$carrd1.click(function(){
-    $carrd1.hide()
+  var $carrd1=$('#card1')
+  $('.btn1').click(function(){
+    $('.btn1').html("<ul class='ul'><li>pizza</li><li>Cheese</li><li>tomato</li><li>pepper</li><li>onion</li><li>Price:20DT</li></ul>");
+  
+  
+});
+var $carrd2=$('#card2')
+$('.btn2').click(function(){
+  $('.btn2').html("<ul class='ul'><li>Humberguer</li><li>Cheese</li><li>Tomato</li><li>Pepper</li><li>Onion</li><li>Meat</li><li>Price:10DT</li></ul>");
+  $('.btn2').click().hide
 
 });
+var $carrd3=$('#card3')
+$('.btn3').click(function(){
+  $('.btn3').html("<ul class='ul'><li>tacos</li><li>cheese sauce</li><li>tomato</li><li>onion</li><li>cutlet</li><li>Price:12DT</li></ul>");
 
+
+
+});
+var $carrd4=$('#card4')
+$('.btn4').click(function(){
+$('.btn4').html("<ul class='ul'><li>chiken</li><li>Price:18DT</li></ul>");
+
+
+});
